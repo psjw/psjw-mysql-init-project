a45c940d9b914bfd5690d64c2cca646146435522
psjw <dlrudtn108@gmail.com>
2024-03-24 19:26:49 +0900

    Step02: findMember

--- src/main/java/com/example/fastcampusmysql/controller/MemberController.java
+++ src/main/java/com/example/fastcampusmysql/controller/MemberController.java
@@ -1,21 +1,28 @@
 package com.example.fastcampusmysql.controller;
 
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.service.MemberReadService;
 import com.example.fastcampusmysql.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
     final private MemberWriteService memberWriteService;
+    final private MemberReadService memberReadService;
 
 
     @PostMapping("/members")
-    public Member register(@RequestBody RegisterMemberCommand command){
+    public Member register(@RequestBody RegisterMemberCommand command) {
         return memberWriteService.create(command);
     }
+
+    @GetMapping("/members/{id}")
+    public MemberDto getMember(@PathVariable Long id) {
+        return memberReadService.getMember(id);
+    }
+
 }
